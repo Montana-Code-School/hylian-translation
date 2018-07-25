3d86100f5994ba6bc847d3ce3bbd64aedcff37cf
Tyler Hofer <tylerhofer@Tylers-MacBook-Pro.local>
2018-07-25 13:12:43 -0600

    changed css on home page, put in some text areas, and added needed pictures

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
 import PostsManager from '@/components/PostsManager'
 import Auth from '@okta/okta-vue'
 
@@ -18,8 +18,8 @@ let router = new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
     },
     {
       path: '/implicit/callback',
